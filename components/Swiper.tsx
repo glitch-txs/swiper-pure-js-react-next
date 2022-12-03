a55297eda7f09f53adb0d67e37a3de7e74a795c8
Glitch <bright.glitch@gmail.com>
2022-12-02 22:46:34 -0300

    swiper done, auto slide left

--- components/Swiper.tsx
+++ components/Swiper.tsx
@@ -1,23 +1,73 @@
 import Image from 'next/image'
-import React from 'react'
-import left from '../public/left.jpg'
-import right from '../public/right.jpg'
+import React, { useRef } from 'react'
+import left from '../public/left.svg'
+import right from '../public/right.svg'
 import s from './Swiper.module.css'
 import { data } from './data'
 
 const Swiper = () => {
 
+    const swiperRef = useRef<HTMLDivElement | null>(null)
+
+    const handleNext = ()=>{
+        if(swiperRef.current && swiperRef.current.children.length > 0){
+
+            const firstChild = swiperRef.current.children[0]
+
+            swiperRef.current.style.transition = `all 0.3s ease-out`
+
+            const childWidth = (swiperRef.current.children[0] as HTMLElement).offsetWidth
+
+            swiperRef.current.style.transform = `translateX(-${childWidth}px)`
+
+            const transition = ()=>{
+                if(swiperRef.current){
+                    swiperRef.current.style.transition = `none`
+                    swiperRef.current.style.transform = `translateX(0px)`
+                    swiperRef.current.appendChild(firstChild)
+
+                    swiperRef.current.removeEventListener('transitionend', transition)
+                }
+            }
+
+            swiperRef.current.addEventListener('transitionend', transition)
+
+        }
+    }
+    
+    const handlePrev = ()=>{
+        if(swiperRef.current && swiperRef.current.children.length > 0){
+
+            const lastChildIndex = swiperRef.current.children.length - 1
+            
+            const childWidth = (swiperRef.current.children[0] as HTMLElement).offsetWidth
+            
+            const lastChild = swiperRef.current.children[lastChildIndex]
+
+            swiperRef.current.insertBefore(lastChild, swiperRef.current.firstChild)
+
+            swiperRef.current.style.transition = 'none'
+            swiperRef.current.style.transform = `translateX(-${childWidth}px)`
+
+            setTimeout(()=>{
+                if(swiperRef.current){
+                    swiperRef.current.style.transition = `all 0.3s ease-out`
+                    swiperRef.current.style.transform = `translateX(0px)`
+                }
+            },0)
+
+        }
+    }
+
   return (
     <div className={s.container} >
 
-        <div className={s.slideContainer} >
+        <div className={s.slideContainer} ref={swiperRef} >
 
             {data?.map((item, index)=>(
                 <div key={index} className={s.slide}>
 
-                    <div>
-                        <Image src={item[0]} alt="" />
-                    </div>
+                    <Image src={item[0]} alt="" quality={100} />
 
                     <p className={s.textSlide} >
                         {item[1]}
@@ -28,11 +78,11 @@ const Swiper = () => {
 
         </div>
 
-        <div>
-            <button>
+        <div className={s.controlsContainer} >
+            <button onClick={handlePrev}>
                 <Image src={left} alt="" />
             </button>
-            <button>
+            <button onClick={handleNext}>
                 <Image src={right} alt="" />
             </button>
         </div>
