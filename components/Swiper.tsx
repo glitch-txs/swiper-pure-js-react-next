8516258d01e558dc2077bf078c5aa7df2f8bdc1a
Glitch <bright.glitch@gmail.com>
2022-12-04 20:00:37 -0300

    draggable added

--- components/Swiper.tsx
+++ components/Swiper.tsx
@@ -10,6 +10,9 @@ const Swiper = () => {
     const animationTime: number = 3000
     const transitionTime: string = '0.3s'
 
+    //The number by which the width of the slide will be divided by to decide where to move when dragging stops.
+    const offsetPresition: number = 3
+
     const swiperRef = useRef<HTMLDivElement | null>(null)
     const containerRef = useRef<HTMLDivElement | null>(null)
     const intervalIDRef = useRef<NodeJS.Timer | null>(null)
@@ -17,18 +20,20 @@ const Swiper = () => {
     const handleNext = ()=>{
         if(swiperRef.current && swiperRef.current.children.length > 0){
 
+            swiperRef.current.style.left = `0px`
+
             const firstChild = swiperRef.current.children[0]
 
-            swiperRef.current.style.transition = `all ${transitionTime} ease-out`
+            swiperRef.current.style.transition = `left ${transitionTime} ease-out`
 
             const childWidth = (swiperRef.current.children[0] as HTMLElement).offsetWidth
 
-            swiperRef.current.style.transform = `translateX(-${childWidth}px)`
+            swiperRef.current.style.left = `-${childWidth}px`
 
             const transition = ()=>{
                 if(swiperRef.current){
                     swiperRef.current.style.transition = `none`
-                    swiperRef.current.style.transform = `translateX(0px)`
+                    swiperRef.current.style.left = `0px`
                     swiperRef.current.appendChild(firstChild)
 
                     swiperRef.current.removeEventListener('transitionend', transition)
@@ -52,12 +57,12 @@ const Swiper = () => {
             swiperRef.current.insertBefore(lastChild, swiperRef.current.firstChild)
 
             swiperRef.current.style.transition = 'none'
-            swiperRef.current.style.transform = `translateX(-${childWidth}px)`
+            swiperRef.current.style.left = `-${childWidth}px`
 
             setTimeout(()=>{
                 if(swiperRef.current){
-                    swiperRef.current.style.transition = `all ${transitionTime} ease-out`
-                    swiperRef.current.style.transform = `translateX(0px)`
+                    swiperRef.current.style.transition = `left ${transitionTime} ease-out`
+                    swiperRef.current.style.left = `0px`
                 }
             },0)
 
@@ -65,118 +70,212 @@ const Swiper = () => {
     }
 
     // //Automatic Change
-    // useEffect(()=>{
-
-    //     if(intervalIDRef.current){
-    //         clearInterval(intervalIDRef.current)
-    //     }
-
-    //     intervalIDRef.current = setInterval(()=>{
-    //         handleNext()
-    //     }, animationTime)
-    
-    //     return ()=> {
-    //         if(intervalIDRef.current){
-    //             clearInterval(intervalIDRef.current)
-    //         }
-    //     }
-
-    // },[])
-
-    // //clear interval when hover - OPTIONAL
-    // useEffect(()=>{
-    //     swiperRef.current?.addEventListener('mouseenter', ()=>{
-    //         if(intervalIDRef.current)
-    //         clearInterval(intervalIDRef.current)
-    //     })
-    //     swiperRef.current?.addEventListener('mouseleave', ()=>{
-    //         intervalIDRef.current = setInterval(()=>{
-    //             handleNext()
-    //         }, animationTime)
-    //     })
-
-    //     //This will stop the animation if the window get blur to stop the animation when the user is not in the website. (Avoids Bugs).
-    //     window.addEventListener('blur', ()=>{
-    //         if(intervalIDRef.current)
-    //         clearInterval(intervalIDRef.current)
-    //     })
-    //     window.addEventListener('focus', ()=>{
-    //         if(intervalIDRef.current)
-    //         clearInterval(intervalIDRef.current)
-    //         intervalIDRef.current = setInterval(()=>{
-    //             handleNext()
-    //         }, animationTime)
-    //     })
-    
-    //     return ()=> {
-    //         //Remove clear interval when hover
-    //         swiperRef.current?.removeEventListener('mouseenter', ()=>{
-    //             if(intervalIDRef.current)
-    //             clearInterval(intervalIDRef.current)
-    //         })
-    //         swiperRef.current?.removeEventListener('mouseleave', ()=>{
-    //             intervalIDRef.current = setInterval(()=>{
-    //                 handleNext()
-    //             }, animationTime)
-    //         })
-    //         window.removeEventListener('blur', ()=>{
-    //             if(intervalIDRef.current)
-    //             clearInterval(intervalIDRef.current)
-    //         })
-    //         window.removeEventListener('focus', ()=>{
-    //             if(intervalIDRef.current)
-    //             clearInterval(intervalIDRef.current)
-    //             intervalIDRef.current = setInterval(()=>{
-    //                 handleNext()
-    //             }, animationTime)
-    //         })
-    //     }
-    // },[])
-    
     useEffect(()=>{
 
-        let isDragStart = false
-        let prevPageX: number
-        let prevScrollLeft: number
-        let positionDiff:number
-
-        const autoSlide = ()=>{
-
+        if(intervalIDRef.current){
+            clearInterval(intervalIDRef.current)
         }
 
-        const dragStart = (e: any)=>{
-            isDragStart = true
-
-            prevPageX = e.pageX || e.touches[0].pageX
-            if(containerRef.current)
-            prevScrollLeft = containerRef.current.scrollLeft
-        }
-
-        const dragStop = ()=>{
-            isDragStart = false
-            autoSlide()
-        }
-
-        const dragging = (e:any)=>{
-            if(containerRef.current && isDragStart){
-                positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX
-
-                console.log(prevScrollLeft - positionDiff)
-                containerRef.current.scrollLeft = prevScrollLeft - positionDiff
+        intervalIDRef.current = setInterval(()=>{
+            handleNext()
+        }, animationTime)
+    
+        return ()=> {
+            if(intervalIDRef.current){
+                clearInterval(intervalIDRef.current)
             }
         }
 
-        if(swiperRef.current && containerRef.current){
-            if(window.innerWidth < 1000){
-                swiperRef.current.addEventListener('touchmove',dragging)
-                containerRef.current.addEventListener('touchstart',dragStart)
-                containerRef.current.addEventListener('touchend',dragStop)
-            }else{
-                swiperRef.current.addEventListener('mousemove',dragging)
-                containerRef.current.addEventListener('mousedown',dragStart)
-                containerRef.current.addEventListener('mouseup',dragStop)
+    },[])
+
+    // //clear interval when hover - OPTIONAL
+    useEffect(()=>{
+        swiperRef.current?.addEventListener('mouseenter', ()=>{
+            if(intervalIDRef.current)
+            clearInterval(intervalIDRef.current)
+        })
+        swiperRef.current?.addEventListener('mouseleave', ()=>{
+            intervalIDRef.current = setInterval(()=>{
+                handleNext()
+            }, animationTime)
+        })
+
+        //This will stop the animation if the window get blur to stop the animation when the user is not in the website. (Avoids Bugs).
+        window.addEventListener('blur', ()=>{
+            if(intervalIDRef.current)
+            clearInterval(intervalIDRef.current)
+        })
+        window.addEventListener('focus', ()=>{
+            if(intervalIDRef.current)
+            clearInterval(intervalIDRef.current)
+            intervalIDRef.current = setInterval(()=>{
+                handleNext()
+            }, animationTime)
+        })
+    
+        return ()=> {
+            //Remove clear interval when hover
+            swiperRef.current?.removeEventListener('mouseenter', ()=>{
+                if(intervalIDRef.current)
+                clearInterval(intervalIDRef.current)
+            })
+            swiperRef.current?.removeEventListener('mouseleave', ()=>{
+                intervalIDRef.current = setInterval(()=>{
+                    handleNext()
+                }, animationTime)
+            })
+            window.removeEventListener('blur', ()=>{
+                if(intervalIDRef.current)
+                clearInterval(intervalIDRef.current)
+            })
+            window.removeEventListener('focus', ()=>{
+                if(intervalIDRef.current)
+                clearInterval(intervalIDRef.current)
+                intervalIDRef.current = setInterval(()=>{
+                    handleNext()
+                }, animationTime)
+            })
+        }
+    },[])
+    
+    //Draggable
+    useEffect(()=>{
+
+        let posX1: number;
+        let posX2: number;
+        let totalDragged = 0;
+
+
+        if(window.innerWidth < 900 && swiperRef.current && containerRef.current){
+            containerRef.current.addEventListener("touchstart", dragStart);
+        } else if(containerRef.current) {
+            containerRef.current.addEventListener("mousedown", dragStart);
+        }
+
+        function dragMove(e:any) {
+
+            if(swiperRef.current){
+            const childWidth = (swiperRef.current.children[0] as HTMLElement).offsetWidth
+
+            if (e.type == "touchmove") {
+                posX2 = posX1 - e.touches[0].clientX;
+                posX1 = e.touches[0].clientX;
+              } else {
+                posX2 = posX1 - e.clientX;
+              }
+            
+              if(totalDragged <= -1 * childWidth && totalDragged >= childWidth){
+                return
+              } else {
+                swiperRef.current.style.left = `-${childWidth + posX2}px`;
+              }
+              totalDragged += posX2
+            }
+
+          }
+          
+        function dragEnd() {
+            if(swiperRef.current && containerRef.current){
+                const childWidth = (swiperRef.current.children[0] as HTMLElement).offsetWidth
+                if (swiperRef.current.offsetLeft + childWidth < -1 * childWidth / offsetPresition && swiperRef.current && swiperRef.current.children.length > 0) {
+                    //Go to Next slide
+
+                    swiperRef.current.style.left = `-${childWidth}px`
+            
+                    const firstChild = swiperRef.current.children[0]
+                    const secondtChild = swiperRef.current.children[1]
+            
+                    swiperRef.current.style.transition = `left ${transitionTime} ease-out`
+            
+                    swiperRef.current.style.left = `-${childWidth * 2}px`
+            
+                    const transition = ()=>{
+                        if(swiperRef.current){
+                            swiperRef.current.style.transition = `none`
+                            swiperRef.current.style.left = `0px`
+                            swiperRef.current.appendChild(firstChild)
+                            swiperRef.current.appendChild(secondtChild)
+            
+                            swiperRef.current.removeEventListener('transitionend', transition)
+                        }
+                    }
+            
+                    swiperRef.current.addEventListener('transitionend', transition)
+
+                } else if (swiperRef.current.offsetLeft + childWidth > childWidth / offsetPresition) {
+                    //Go to Prev slide
+                    swiperRef.current.style.transition = `left ${transitionTime} ease-out`
+                    swiperRef.current.style.left = `0px`
+                } else {
+                    //Stay in the current slide
+                    const transition = ()=>{
+                        if(swiperRef.current){
+                            const firstChild = swiperRef.current.children[0]
+                            swiperRef.current.style.transition = `none`
+                            swiperRef.current.style.left = `0px`
+                            swiperRef.current.appendChild(firstChild)
+            
+                            swiperRef.current.removeEventListener('transitionend', transition)
+                        }
+                    }
+                    //transition won't trigger if distance is 0
+                    if(swiperRef.current.offsetLeft + childWidth == 0){
+                        const firstChild = swiperRef.current.children[0]
+                        swiperRef.current.style.left = `0px`
+                        swiperRef.current.appendChild(firstChild)
+                    } else {
+                        swiperRef.current.style.transition = `left ${transitionTime} ease-out`
+                        swiperRef.current.style.left = `-${childWidth}px`
+                        swiperRef.current.addEventListener('transitionend', transition)
+                    }
+                }
+
+                swiperRef.current.removeEventListener("touchmove", dragMove)
+                containerRef.current.removeEventListener("touchend", dragEnd)
+                containerRef.current.removeEventListener("mouseup", dragEnd)
+                containerRef.current.removeEventListener("mousemove", dragMove)
             }
         }
+
+        function dragStart(e:any) {
+            if(containerRef.current && swiperRef.current){
+                swiperRef.current.style.transition = 'none'
+
+                //Add prev image in case is dragedd to left
+                const lastChildIndex = swiperRef.current.children.length - 1
+            
+                const childWidth = (swiperRef.current.children[0] as HTMLElement).offsetWidth
+                
+                const lastChild = swiperRef.current.children[lastChildIndex]
+                swiperRef.current.insertBefore(lastChild, swiperRef.current.firstChild)
+
+                swiperRef.current.style.transition = 'none'
+                swiperRef.current.style.left = `-${childWidth}px`
+                //
+
+                if (e.type == "touchstart") {
+                  posX1 = e.touches[0].clientX;
+
+                  swiperRef.current.addEventListener("touchmove", dragMove);
+                  containerRef.current.addEventListener("touchend", dragEnd);
+                } else {
+                  posX1 = e.clientX;
+              
+                  containerRef.current.addEventListener("mouseup", dragEnd);
+                  containerRef.current.addEventListener("mousemove", dragMove);
+                }
+            }            
+        }
+
+        return ()=>{
+            if(swiperRef.current && containerRef.current){
+                swiperRef.current.removeEventListener("touchmove", dragMove)
+                containerRef.current.removeEventListener("touchend", dragEnd)
+                containerRef.current.removeEventListener("mouseup", dragEnd)
+                containerRef.current.removeEventListener("mousemove", dragMove)
+            }
+        }
+
     },[])
 
   return (
